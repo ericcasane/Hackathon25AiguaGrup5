7c771c09e873da234993bcfc7ec359d2ec15034b
scrutcm <ecasane@edu.tecnocampus.cat>
2025-04-05 01:06:10 +0200

    home and welcome message

--- App/app/src/main/java/com/tecnocampus/aquaactua/MainActivity.java
+++ App/app/src/main/java/com/tecnocampus/aquaactua/MainActivity.java
@@ -1,6 +1,7 @@
 package com.tecnocampus.aquaactua;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public void showBottomNavigationView() {
+        if (bottomNavigationView != null) {
+            bottomNavigationView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void loadRegisterFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .setCustomAnimations(R.anim.slide_up, R.anim.slide_down, 0, 0)
+                .add(R.id.fragmentContainer, new RegisterFragment())
+                .addToBackStack(null)
+                .commit();
+    }
+
+    public void loadSignInFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, new LogInFragment())
+                .addToBackStack(null)
+                .commit();
+    }
+
+    public void loadHomeFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, new HomeFragment())
+                .commit();
+        showBottomNavigationView();
     }
 }
---
+++ App/app/src/main/java/com/tecnocampus/aquaactua/WelcomeFragment.java
@@ -0,0 +1,53 @@
+package com.tecnocampus.aquaactua;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+public class WelcomeFragment extends Fragment implements View.OnClickListener {
+
+    public WelcomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        // Find the buttons and set the onClickListener
+        Button btRegister = view.findViewById(R.id.btCreateAccount);
+        Button btLogin = view.findViewById(R.id.btGoToLogin);
+        Button btSkipNow = view.findViewById(R.id.btSkipNow);
+
+        btRegister.setOnClickListener(this);
+        btLogin.setOnClickListener(this);
+        btSkipNow.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button b = (Button) v;
+        MainActivity mainActivity = (MainActivity) getActivity();
+        if (mainActivity != null) {
+            if (b.getId() == R.id.btCreateAccount)
+                mainActivity.loadRegisterFragment();
+            if (b.getId() == R.id.btGoToLogin)
+                mainActivity.loadSignInFragment();
+            else if (b.getId() == R.id.btSkipNow)
+                mainActivity.loadHomeFragment();
+        }
+    }
+}
