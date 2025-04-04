7c9ad645f1f363bf19723f68c4178e7551e46a62
scrutcm <ecasane@edu.tecnocampus.cat>
2025-04-04 22:58:14 +0200

    Create spring project

---
+++ Backend/AquaActua/src/main/java/org/tecnocampus/aquaactua/AquaActuaApplication.java
@@ -0,0 +1,13 @@
+package org.tecnocampus.aquaactua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaActuaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquaActuaApplication.class, args);
+    }
+
+}
---
+++ Backend/AquaActua/src/test/java/org/tecnocampus/aquaactua/AquaActuaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tecnocampus.aquaactua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaActuaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
